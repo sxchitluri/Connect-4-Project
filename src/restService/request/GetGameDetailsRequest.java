f9e333146db713b19ab9180875440ae394217217
Sriharshini Chitluri <srichitluri@gmail.com>
2024-04-20 18:11:04 -0400

    made the request def for story 2 game deets file

--- src/restService/request/GetGameDetailsRequest.java
+++ src/restService/request/GetGameDetailsRequest.java
@@ -3,34 +3,27 @@ package restService.request;
 import DataAccess.BoardDataAccess;
 import DataObjects.BoardDataObject;
 
+//story 2 request definition
 public class GetGameDetailsRequest {
 
     private final int gameId;
-    private final int gameTypeId;
-    private final int player1Id;
-    private final int player2Id;
-    private final String status;
-    private final int currentTurnPlayer;
-    private final int winnerId;
-    private final BoardDataObject board;
+    // private final int gameTypeId;
+    // private final int player1Id;
+    // private final int player2Id;
+    // private final String status;
+    // private final int currentTurnPlayer;
+    // private final int winnerId;
+    // private final BoardDataObject board;
 
     //GET BACK TO DOING THIS AND ADDING ALL THE GETS FOR EACH VARIABLE
 
-    // default playgame request that has default values and creates a new board?????????????????
-    public PlayGameRequest(int gameId, int gameTypeId, int player1Id, int player2Id, String status,
-            int currentTurnPlayer, int winnerId, BoardDataObject board) {
+    // getgamedetails request that takes in only the gameId
+    public GetGameDetailsRequest (int gameId) {
         this.gameId = gameId;
-        this.gameTypeId = gameTypeId;
-        this.player1Id = player1Id;
-        this.player2Id = player2Id;
-        this.currentTurnPlayer = currentTurnPlayer; 
-        this.status = status; 
-        this.winnerId = winnerId; 
-        this.board = board;
     }
 
-    public int GetGameTypeId() {
-        return this.gameTypeId;
+    public int GetGameId() {
+        return this.gameId;
     }
 
 }
@@ -51,4 +44,4 @@ public class GetGameDetailsRequest {
  */
 
     
-}
+
