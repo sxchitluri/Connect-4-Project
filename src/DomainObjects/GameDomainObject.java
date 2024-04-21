65fd6b3c410ed4de8170bd68030b81948d554aa8
Sriharshini Chitluri <srichitluri@gmail.com>
2024-04-21 19:14:06 -0400

    fixes on the board method at the end

--- src/DomainObjects/GameDomainObject.java
+++ src/DomainObjects/GameDomainObject.java
@@ -1,4 +1,5 @@
 package DomainObjects;
+
 import java.util.ArrayList;
 import DataObjects.GameDataObject;
 import Models.GameModel;
@@ -6,16 +7,17 @@ import Models.BoardModel;
 
 public class GameDomainObject {
     private int gameId;
-    //private int gameTypeId;
+    // private int gameTypeId;
     private int player1Id;
     private int player2Id;
     private int currentTurnPlayer;
     private String status;
     private int winnerId;
-    //private String board;
+    // private String board;
     private BoardDomainObject board;
 
-    public GameDomainObject (int gameId, int player1Id, int player2Id, int currentTurnPlayer, String status, int winnerId, BoardDomainObject board) {
+    public GameDomainObject(int gameId, int player1Id, int player2Id, int currentTurnPlayer, String status,
+            int winnerId, BoardDomainObject board) {
         this.gameId = gameId;
         this.player1Id = player1Id;
         this.player2Id = player2Id;
@@ -25,16 +27,16 @@ public class GameDomainObject {
         this.board = board;
     }
 
-    //Copy Constructor
+    // Copy Constructor
     public GameDomainObject(GameDataObject game) {
         this.gameId = game.id;
-        //this.gameTypeId = game.gameTypeId;
+        // this.gameTypeId = game.gameTypeId;
         this.player1Id = game.player1Id;
         this.player2Id = game.player2Id;
         this.currentTurnPlayer = game.currentTurnPlayer;
         this.status = game.status;
         this.winnerId = game.winnerId;
-        //this.board = game.board;
+        // this.board = game.board;
     }
 
     public static ArrayList<GameDomainObject> MapList(ArrayList<GameDataObject> gamedata) {
@@ -44,7 +46,7 @@ public class GameDomainObject {
         }
         return gameDomain;
     }
-    
+
     public int GetGameId() {
         return this.gameId;
     }
@@ -52,36 +54,38 @@ public class GameDomainObject {
     public int GetPlayer1Id() {
         return this.player1Id;
     }
-    
+
     public int GetPlayer2Id() {
         return this.player2Id;
     }
-    
+
     public int GetCurrentTurnPlayer() {
         return this.currentTurnPlayer;
     }
-    
+
     public String GetStatus() {
         return this.status;
     }
-    
+
     public int GetWinnerId() {
         return this.winnerId;
     }
-    
+
     public BoardDomainObject GetBoard() {
-        //Lazy Load the Rack
+        // Lazy Load the Rack
         if (this.board == null) {
-            this.board = BoardModel.GetBoardById(board.id); //to clarify, do we want this to get the board based off of gameid or boardid?
+            this.board = BoardModel.GetBoardById(board.GetId());
         }
         return this.board;
     }
 
-    //?????
-    /*public void SetBoard(int id) {
-        this.id = id;
-        GameModel.Save(this);
-    }*/
+    // ?????
+    /*
+     * public void SetBoard(int id) {
+     * this.id = id;
+     * GameModel.Save(this);
+     * }
+     */
 
     public void setBoard(BoardDomainObject board) {
         if (board == null) {
