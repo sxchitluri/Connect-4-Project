88b9f4501762466b9cc5f7927a6df6d8c636e932
Sriharshini Chitluri <srichitluri@gmail.com>
2024-04-20 20:46:19 -0400

    PlayerResponse.java should be done

--- src/restService/response/PlayerResponse.java
+++ src/restService/response/PlayerResponse.java
@@ -1,40 +1,65 @@
 package restService.response;
 
+// story 1's reponse definiton 
 public class PlayerResponse {
     
     private final boolean isValid;
     private final String errorMessage;
     private final int playerId;
     private final String username;
+    private final int gamesPlayed;
+    private final int gamesWon;
+    private final int gamesLost;
 
-    public PlayerResponse(int playerId, String username) {
-        this.playerId = playerId;
-        this.username = username;
+    public PlayerResponse(boolean isValid, String errorMessage, int playerId, String username, int gamesPlayed, int gamesWon, int gamesLost) {
         this.isValid = true;
         this.errorMessage = null;
+        this.playerId = playerId;
+        this.username = username;
+        this.gamesPlayed = gamesPlayed;
+        this.gamesWon = gamesWon;
+        this.gamesLost = gamesLost;
     }
 
     public PlayerResponse(String errorMessage) {
-        this.playerId = -1;
-        this.username = "";
         this.isValid = false;
         this.errorMessage = errorMessage;
+        this.playerId = -1;
+        this.username = "";
+        this.gamesPlayed = -1;
+        this.gamesWon = -1;
+        this.gamesLost = -1;
     }
 
-    public int getPlayerId() {
-        return playerId;
-    }
+    //GET methods
 
-    public String getUsername() {
-        return username;
-    }
-
-     public boolean GetIsValid() {
+    public boolean GetIsValid() {
         return this.isValid;
     }
 
     public String GetErrorMessage() {
         return this.errorMessage;
     }
+
+    public int getPlayerId() {
+        return this.playerId;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public int getGamesPlayed() {
+        return this.gamesPlayed;
+    }
+
+    public int getGamesWon() {
+        return this.gamesWon;
+    }
+
+    public int getGamesLost() {
+        return this.gamesLost;
+    }
+     
 
 }
