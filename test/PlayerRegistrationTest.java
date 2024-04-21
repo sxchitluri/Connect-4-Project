5759c0ce07bc81206c3ec06a8f54b7c52bf5d69d
Sriharshini Chitluri <srichitluri@gmail.com>
2024-04-21 16:57:04 -0400

    add tests to PlayerRegistrationTest.java

--- test/PlayerRegistrationTest.java
+++ test/PlayerRegistrationTest.java
@@ -7,23 +7,134 @@ import restService.response.PlayerResponse;
 
 public class PlayerRegistrationTest {
 
+    // tests for valid inputs
     @Test
     public void PlayerController_RegisterPlayer_SuccessTest() {
-        
-        String username = "username";
-        String password = "password";
 
-        //create the request to register player
+        String username = "username3";
+        String password = "password1";
+
+        // create the request to register player
         RegisterPlayerRequest request = new RegisterPlayerRequest(username, password);
 
-        //now call the register player controller and pass in the request to the controller. Then it should return back a reponse, the player response
+        // now call the register player controller and pass in the request to the
+        // controller. Then it should return back a reponse, the player response
         PlayerResponse response = PlayerController.registerPlayer(request);
 
-        //now we test the reponse
+        // now we test the reponse
         assertEquals(true, response.GetIsValid());
         assertEquals(null, response.GetErrorMessage());
-        assertEquals(username, response.getUsername());
-        assertEquals(?, response.getPlayerId());
+        assertEquals("username3", response.getUsername());
+        assertEquals(1, response.getPlayerId());
+        // do we need an assertEquals for password?, if we are not storing them here?
+    }
+
+    @Test
+    public void PlayerController_RegisterPlayer_SuccessTest_SeqPlayerId() {
+
+        String username = "username4";
+        String password = "password2";
+
+        RegisterPlayerRequest request = new RegisterPlayerRequest(username, password);
+        PlayerResponse response = PlayerController.registerPlayer(request);
+
+        // now we test the reponse
+        assertEquals(true, response.GetIsValid());
+        assertEquals(null, response.GetErrorMessage());
+        assertEquals("username4", response.getUsername());
+        assertEquals(2, response.getPlayerId());
+
+    }
+
+    // tests for invalid inputs
+    @Test
+    public void PlayerController_RegisterPlayer_UsernameShort() {
+        String username = "short";
+        String password = "tenchars00";
+
+        RegisterPlayerRequest request = new RegisterPlayerRequest(username, password);
+        PlayerResponse response = PlayerController.registerPlayer(request);
+
+        // now we test the reponse
+        assertEquals(false, response.GetIsValid());
+        assertEquals("Invalid Username", response.GetErrorMessage());
+        assertEquals(null, response.getUsername());
+        assertEquals(0, response.getPlayerId());
+    }
+
+    @Test
+    public void PlayerController_RegisterPlayer_UsernameLong() {
+        String username = "Immunotherapeutically";
+        String password = "tenchars00";
+
+        RegisterPlayerRequest request = new RegisterPlayerRequest(username, password);
+        PlayerResponse response = PlayerController.registerPlayer(request);
+
+        // now we test the reponse
+        assertEquals(false, response.GetIsValid());
+        assertEquals("Invalid Username", response.GetErrorMessage());
+        assertEquals(null, response.getUsername());
+        assertEquals(0, response.getPlayerId());
+    }
+
+    @Test
+    public void PlayerController_RegisterPlayer_UsernameInvalidChar() {
+        String username = "Rainbow!!!!!";
+        String password = "tenchars00";
+
+        RegisterPlayerRequest request = new RegisterPlayerRequest(username, password);
+        PlayerResponse response = PlayerController.registerPlayer(request);
+
+        // now we test the reponse
+        assertEquals(false, response.GetIsValid());
+        assertEquals("Invalid Username", response.GetErrorMessage());
+        assertEquals(null, response.getUsername());
+        assertEquals(0, response.getPlayerId());
+    }
+
+    @Test
+    public void PlayerController_RegisterPlayer_UsernameUnique() {
+        String username = "doomsmith"; // already made in playerdataaccess
+        String password = "tenchars00";
+
+        RegisterPlayerRequest request = new RegisterPlayerRequest(username, password);
+        PlayerResponse response = PlayerController.registerPlayer(request);
+
+        // now we test the reponse
+        assertEquals(false, response.GetIsValid());
+        assertEquals("Username is not unique", response.GetErrorMessage());
+        assertEquals(null, response.getUsername());
+        assertEquals(0, response.getPlayerId());
+    }
+
+    @Test
+    public void PlayerController_RegisterPlayer_PasswordShort() {
+        String username = "username1";
+        String password = "tent";
+
+        RegisterPlayerRequest request = new RegisterPlayerRequest(username, password);
+        PlayerResponse response = PlayerController.registerPlayer(request);
+
+        // now we test the reponse
+        assertEquals(false, response.GetIsValid());
+        assertEquals("Invalid Password", response.GetErrorMessage());
+        assertEquals("username1", response.getUsername());
+        assertEquals(0, response.getPlayerId());
+    }
+
+    @Test
+    public void PlayerController_RegisterPlayer_PasswordLong() {
+        String username = "username2";
+        String password = "passwordistoolong1234";
+
+        RegisterPlayerRequest request = new RegisterPlayerRequest(username, password);
+        PlayerResponse response = PlayerController.registerPlayer(request);
+
+        // now we test the reponse
+        assertEquals(false, response.GetIsValid());
+        assertEquals("Invalid Password", response.GetErrorMessage());
+        assertEquals("username2", response.getUsername());
+        assertEquals(0, response.getPlayerId());
     }
 
 }
