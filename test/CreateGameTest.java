21986d31b1ce3b09454330c7af4151fe7ccb46ed
Sriharshini Chitluri <srichitluri@gmail.com>
2024-04-21 22:29:33 -0400

    Made new test files for story 3 and 5

---
+++ test/CreateGameTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+import Controller.GameController;
+import restService.request.PlayGameRequest;
+import restService.response.GameResponse;
+
+// Story 3 test
+
+public class CreateGameTest {
+
+}
---
+++ test/PlayGameWinnerTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+import Controller.GameController;
+import restService.request.PlayGameRequest;
+import restService.response.GameResponse;
+
+// Story 5 test
+public class PlayGameWinnerTest {
+
+}
