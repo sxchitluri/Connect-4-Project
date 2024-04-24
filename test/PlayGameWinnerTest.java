f0927598960c2d9641cff476409fa9311654d2fc
Sriharshini Chitluri <srichitluri@gmail.com>
2024-04-23 20:35:11 -0400

    added scenario # comments

--- test/PlayGameWinnerTest.java
+++ test/PlayGameWinnerTest.java
@@ -29,54 +29,61 @@ public class PlayGameWinnerTest {
     RegisterPlayerRequest requestP3 = new RegisterPlayerRequest("ttpoetsdept", "smashriptear");
     PlayerResponse responseP3 = PlayerController.registerPlayer(requestP3);
 
+    // Scenario 5.1: gameid does not exist - input not found
     @Test
     public void testGameWithInvalidId() {
-        GameResponse response = GameController.GetGame(943);  // Non-existing ID
+        GameResponse response = GameController.GetGame(943); // Non-existing ID
         assertEquals(false, response.GetIsValid());
         assertEquals("Invalid GameId.", response.GetErrorMessage());
     }
 
     @Test
-    /*public void testGamePlayNoWin() {
-        CreateGameRequest createRequest = new CreateGameRequest(1, 2, 0);
-        GameResponse response = GameController.CreateGame(createRequest);
-    
-        // Now reuse the same 'response' variable for the next call
-        response = GameController.PlayGame(new PlayGameRequest(251, 1, 3));  // Valid move
-    
-        assertEquals(true, response.GetIsValid());
-        assertNull(response.GetErrorMessage());
-        assertEquals("Playing", response.GetStatus());
-        assertEquals(0, response.GetWinnerId());
-    }*/
+    /*
+     * public void testGamePlayNoWin() {
+     * CreateGameRequest createRequest = new CreateGameRequest(1, 2, 0);
+     * GameResponse response = GameController.CreateGame(createRequest);
+     * 
+     * // Now reuse the same 'response' variable for the next call
+     * response = GameController.PlayGame(new PlayGameRequest(251, 1, 3)); // Valid
+     * move
+     * 
+     * assertEquals(true, response.GetIsValid());
+     * assertNull(response.GetErrorMessage());
+     * assertEquals("Playing", response.GetStatus());
+     * assertEquals(0, response.GetWinnerId());
+     * }
+     */
 
+    // Scenario 5.2.1: gameid does exist - input found but no winner yet
     public void testGamePlayNoWin() {
         // Create a game request
         CreateGameRequest createRequest = new CreateGameRequest(1, 2, 0);
         // Create the game
         GameResponse createGameResponse = GameController.CreateGame(createRequest);
-    
+
         // Print details about the created game
         System.out.println("Created Game ID: " + createGameResponse.GetGameId());
         System.out.println("Game Creation IsValid: " + createGameResponse.GetIsValid());
         System.out.println("Game Creation Error Message: " + createGameResponse.GetErrorMessage());
-    
+
         // Retrieve the game ID from the created game response
-        int gameId = createGameResponse.GetGameId();  
-        
+        int gameId = createGameResponse.GetGameId();
+
         // Play the game with the retrieved game ID
-        GameResponse response = GameController.PlayGame(new PlayGameRequest(gameId, 2, 3));  
-    
+        GameResponse response = GameController.PlayGame(new PlayGameRequest(gameId, 2, 3));
+
         // Debugging output to check the response of playing the game
         System.out.println("Game ID used for PlayGame: " + gameId);
         System.out.println("IsValid: " + response.GetIsValid());
         System.out.println("Error Message: " + response.GetErrorMessage());
-        
+
         // Assertions to verify the game play results
         assertEquals(true, response.GetIsValid());
         assertNull(response.GetErrorMessage());
         assertEquals("Playing", response.GetStatus());
         assertEquals(0, response.GetWinnerId());
     }
+
+    // Scenario 5.2.2: gameid does exist - input found and there is a winner
 
 }
