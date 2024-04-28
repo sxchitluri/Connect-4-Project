bf34c21e22b4feaa25d6bee6265943c458092a54
Sriharshini Chitluri <srichitluri@gmail.com>
2024-04-28 18:17:17 -0400

    added the invalid input test for getgamedetails

--- test/GetGameDetailsTest.java
+++ test/GetGameDetailsTest.java
@@ -19,7 +19,7 @@ public class GetGameDetailsTest {
     // create a game here with gameid=0
 
     // tests for valid inputs
-    // Scenario
+    // Scenario 2.2: gameid does exist - input found
     @Test
     public void GameController_GetGameDetails_SuccessTest() {
 
@@ -45,6 +45,34 @@ public class GetGameDetailsTest {
         assertEquals(0, response.GetWinnerId());
         assertNotNull(response.GetBoard());
         assertEquals("                                          ", response.GetBoard());
+    }
+
+    // tests for invalid inputs
+    // Scenario 2.1: gameid does not exist - input not found
+    @Test
+    public void GameController_GetGameDetails_NotExistTest() {
+
+        int gameid = 0;
+
+        // create the request to get game details
+        GetGameDetailsRequest request = new GetGameDetailsRequest(gameid);
+
+        // now call the game controller's getgamedetails method and pass in the
+        // request to the
+        // controller. Then it should return back a reponse, the game response
+        GameResponse response = GameController.getGameDetails(request);
+
+        // now we test the reponse
+        assertEquals(true, response.GetIsValid());
+        assertNull(response.GetErrorMessage());
+        assertEquals(-1, response.GetGameId());
+        assertEquals(-1, response.GetGameTypeId());
+        assertEquals(-1, response.GetPlayer1Id());
+        assertEquals(-1, response.GetPlayer2Id());
+        assertEquals(-1, response.GetCurrentTurnPlayer());
+        assertEquals("", response.GetStatus());
+        assertEquals(-1, response.GetWinnerId());
+        assertEquals("", response.GetBoard());
     }
 
 }
