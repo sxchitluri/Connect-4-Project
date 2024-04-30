65714af04b4d9c00297c51299a372521f59ccd94
LBrandGS <162302687+LBrandGS@users.noreply.github.com>
2024-04-29 21:13:26 -0400

    Update GameDataObject.java

--- src/DataObjects/GameDataObject.java
+++ src/DataObjects/GameDataObject.java
@@ -11,7 +11,7 @@ public class GameDataObject {
     public String status;
     public int currentTurnPlayer;
     public int winnerId;
-    public BoardDomainObject board;
+    public BoardDomainObject board;  // Ensure this is always handled
 
     public GameDataObject(int id, int player1Id, int player2Id, String status, int currentTurnPlayer, int winnerId) {
         this.id = id;
@@ -22,15 +22,14 @@ public class GameDataObject {
         this.winnerId = winnerId;
     }
 
-    public GameDataObject(int id, int player1Id, int player2Id, String status, int currentTurnPlayer, int winnerId,
-            BoardDomainObject board) {
+    public GameDataObject(int id, int player1Id, int player2Id, String status, int currentTurnPlayer, int winnerId, BoardDomainObject board) {
         this.id = id;
         this.player1Id = player1Id;
         this.player2Id = player2Id;
         this.status = status;
         this.currentTurnPlayer = currentTurnPlayer;
         this.winnerId = winnerId;
-        this.board = board;
+        this.board = board;  // Set board here
     }
 
     // Copy Constructor
@@ -51,6 +50,6 @@ public class GameDataObject {
         this.status = game.GetStatus();
         this.currentTurnPlayer = game.GetCurrentTurnPlayer();
         this.winnerId = game.GetWinnerId();
-        this.board = game.GetBoard();
+        this.board = game.GetBoard();  // Ensure board is set here
     }
 }
